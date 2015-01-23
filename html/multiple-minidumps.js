57a5d43094b8cc4d5fe8cd09f822e13e4b2f9af2
Benjamin Smedberg <benjamin@smedbergs.us>
2015-01-23 18:58:57 -0500

    Expansion and highlighting.

--- html/multiple-minidumps.js
+++ html/multiple-minidumps.js
@@ -31,11 +31,22 @@ function appendDump(name, dump, row) {
 
   for (var thread_no in dump.threads) {
     var thread = dump.threads[thread_no];
-    var irow = table.append("tr");
-    irow.append("td").attr("rowspan", thread.frames.length + 1).text(thread_no);
+
+    var crashed = dump.crash_info.crashing_thread == thread_no;
+
+    var irow = table.append("tr").classed("expandable", true);
+    irow.append("td")
+      .attr("rowspan", thread.frames.length + 1)
+      .text(thread_no);
+    if (crashed) {
+      irow.classed("expanded", true);
+    }
     for (var frame_no in thread.frames) {
       var frame = thread.frames[frame_no];
-      irow = table.append("tr");
+      irow = table.append("tr").classed("highlightable", true);
+      if (frame_no != 0 && !crashed) {
+        irow.classed("hidden", true);
+      }
       irow.append("td").text(frame_no);
       makeFrameCell(frame, irow.append("td"));
     }
@@ -52,6 +63,8 @@ function makeFrameCell(frame, cell) {
     label = "@" + frame.offset;
   }
   cell.text(label);
+  cell.classed("location", true);
+  cell.attr("title", label);
   if (frame.missing_symbols) {
     cell.classed("nosymbols", true);
   }
@@ -60,3 +73,42 @@ function makeFrameCell(frame, cell) {
   }
   return cell;
 }
+
+function closest(el, classname) {
+  while (el) {
+    if (el.classList.contains(classname)) {
+      return el;
+    }
+    el = el.parentElement;
+  }
+  return null;
+}
+
+d3.select(document).on("click.expand", function() {
+  var el = closest(d3.event.target, "expandable");
+  console.log("xpclick", el);
+  if (!el) {
+    return;
+  }
+
+  var expanded = !el.classList.contains("expanded");
+  el.classList.toggle("expanded", expanded);
+
+
+  el = el.nextElementSibling;
+  el = el && el.nextElementSibling;
+  while (el && !el.classList.contains("expandable")) {
+    console.log("changing visibility", el);
+    el.classList.toggle("hidden", !expanded);
+    el = el.nextElementSibling;
+  }
+});
+
+d3.select(document).on("click.highlight", function() {
+  var el = closest(d3.event.target, "highlightable");
+  if (!el) {
+    return;
+  }
+
+  el.classList.toggle("highlighted");
+});
